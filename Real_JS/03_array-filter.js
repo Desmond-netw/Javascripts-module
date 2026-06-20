3320746930dd60ffa72585e2e1e5a9927a2d1785
Desmond-netw <dsquare200@gmail.com>
2026-06-20 18:12:05 +0300

    array filter

--- Real_JS/03_array-filter.js
+++ Real_JS/03_array-filter.js
@@ -4,6 +4,24 @@ function filterOutOddNumbers (numbers) {
     return numbers.filter(function (number){
         return number % 2 === 0;
     })
+};
+
+function filterObjectsByNameLength(objects, maxLength) {
+    return objects.filter(function (person){
+        return person.name.length <= maxLength;
+    });
+}
+
+function compoundFilter(products) {
+  return products.filter(function (product) {
+    return (
+      typeof product.code === "string" &&
+      product.code.length > 5 &&
+      !product.category.includes("special") &&
+      product.price > 50 &&
+      product.location !== "Underground"
+    );
+  });
 }
 
 const objects = [
